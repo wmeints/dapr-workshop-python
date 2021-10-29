179d82887f1ead47fd8406bc776270308f82f69a
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-10-29 13:04:19 +0200

    Add code for start state of exercise 4

--- FineCollectionService/fine_collection/__init__.py
+++ FineCollectionService/fine_collection/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import Response
 from . import models, settings, services, clients
 from os import environ
@@ -15,8 +15,20 @@ processor = services.ViolationProcessor(
 )
 
 
+@app.get("/dapr/subscribe")
+def subscribe():
+   subscription = [dict(
+      pubsubname="pubsub",
+      topic="speedingviolations",
+      route="/collectfine"
+   )]
+
+   return subscription
+
+
 @app.post("/collectfine")
-def collect_fine(violation: models.SpeedingViolation) -> Response:
+def collect_fine(evt_data=Body(...)) -> Response:
+    violation = models.SpeedingViolation.parse_raw(evt_data["data"])
     processor.process_speed_violation(violation)
 
     return Response(status_code=200)
