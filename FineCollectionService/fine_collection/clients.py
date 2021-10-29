a0dcfc5bd523d4e4a3c2da85ee67dafd0d408988
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-10-29 12:57:10 +0200

    Add code for start state of assignment 3

--- FineCollectionService/fine_collection/clients.py
+++ FineCollectionService/fine_collection/clients.py
@@ -1,30 +1,27 @@
-import requests
 from pydantic import BaseModel
-
-
-class ClientError(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
+from dapr.clients import DaprClient
 
 
 class Vehicle(BaseModel):
-    vehicleId: str
-    make: str
-    model: str
-    ownerName: str
-    ownerEmail: str
+   vehicleId: str
+   make: str
+   model: str
+   ownerName: str
+   ownerEmail: str
 
 
 class VehicleRegistrationClient:
-    def __init__(self, base_address: str):
-        self.base_address = base_address
+   def __init__(self, base_address: str):
+      self.base_address = base_address
 
-    def get_vehicle_info(self, license_number: str) -> Vehicle:
-        response = requests.get(f"{self.base_address}/vehicleinfo/{license_number}")
+   def get_vehicle_info(self, license_number: str) -> Vehicle:
+      with DaprClient() as client:
+            response = client.invoke_method(
+               "vehicleregistrationservice",
+               f"vehicleinfo/{license_number}",
+               data=b'',
+               http_verb="GET"
+            )
 
-        if response.status_code == 200:
-            return Vehicle.parse_raw(response.content)
+            return Vehicle.parse_raw(response.text())
 
-        raise ClientError(
-            f"Failed to retrieve vehicle data. Got status {response.status_code}: {str(response.content)}"
-        )
